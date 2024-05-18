42708f93c3ffa072838f698938b8f7df93d8cc1d
AlexKerpy <4905338@gmail.com>
2024-05-18 09:26:50 +0300

    Added the cpp file with map instructions

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int main(){
+    cout << "Ways to create a map container:\n\n";
+    cout << "Standard: \n";
+    map<int, string> mp;
+    map<int, string> mp1;
+    map<int, string>::const_iterator iter;
+    mp[1] = "one";
+    mp[2] = "two";
+    mp[3] = "three";
+    
+    for(iter = mp.begin(); iter != mp.end(); iter++){
+        cout << "Key: " << iter->first << " Value: " << iter->second << endl;
+    }
+
+    cout << "\n\nBy an insert() and make_pair() funcs:\n";
+    mp1.insert(make_pair(4, "four"));
+    mp1.insert(make_pair(5, "five"));
+    mp1.insert(make_pair(6, "six"));
+
+    for(iter = mp1.begin(); iter != mp1.end(); iter++){
+        cout << "Key: " << iter->first << " Value : " << iter->second << endl;
+    }
+    
+
+    return 0;
+}
