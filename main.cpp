78e50631645c1de4038d52176df776466de1852d
Alex <4905338@gmail.com>
2024-05-23 21:00:42 +0300

    Added one string

--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
     cout << "Ways to create a map container:\n\n";
     cout << "Standard: \n";
     map<int, string> mp;
@@ -13,8 +14,9 @@ int main(){
     mp[1] = "one";
     mp[2] = "two";
     mp[3] = "three";
-    
-    for(iter = mp.begin(); iter != mp.end(); iter++){
+
+    for (iter = mp.begin(); iter != mp.end(); iter++)
+    {
         cout << "Key: " << iter->first << " Value: " << iter->second << endl;
     }
 
@@ -23,10 +25,11 @@ int main(){
     mp1.insert(make_pair(5, "five"));
     mp1.insert(make_pair(6, "six"));
 
-    for(iter = mp1.begin(); iter != mp1.end(); iter++){
+    for (iter = mp1.begin(); iter != mp1.end(); iter++)
+    {
         cout << "Key: " << iter->first << " Value : " << iter->second << endl;
+        mp1.insert(make_pair(10, "ten"));
     }
-    
 
     return 0;
 }
